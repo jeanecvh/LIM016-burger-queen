ae01c412df41544f273d7094b53934b22d90ada3
Brisset <corcinobrisset@gmail.com>
2022-02-28 15:24:29 -0500

    cambios en profile

--- ice-queen/src/app/app-routing.module.ts
+++ ice-queen/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../app/pages/login/login.component'
+import { ProfileComponent } from '../app/components/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'home', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ice-queen/src/app/components/components.module.ts
+++ ice-queen/src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './header/header.component';
 import { InputComponent } from './input/input.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     HeaderComponent,
     InputComponent,
     FooterComponent,
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
@@ -23,7 +25,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   exports: [
     HeaderComponent,
     InputComponent,
-    FooterComponent
+    FooterComponent,
+    ProfileComponent
   ]
 })
 export class ComponentsModule { }
