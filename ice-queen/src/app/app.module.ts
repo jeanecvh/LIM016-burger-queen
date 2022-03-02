600d05da5d28d337d36a4c6228bff2690fbcc598
jeanellach <jeanellach@gmail.com>
2022-03-02 17:58:43 -0500

    change name of the pagesModules(old) to loginModule(new)

--- ice-queen/src/app/app.module.ts
+++ ice-queen/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { ComponentsModule } from './components/components.module';
-import { PagesModule } from './login/pages.module'
+import { LoginModule } from './login/login.module'
 
 
 @NgModule({
@@ -18,7 +18,7 @@ import { PagesModule } from './login/pages.module'
     MaterialModule,
     BrowserAnimationsModule,
     ComponentsModule,
-    PagesModule,
+    LoginModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ice-queen/src/app/login/login.module.ts
+++ ice-queen/src/app/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from '../login/login.component';
+import { LoginComponent } from './login.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 
@@ -25,4 +25,4 @@ import { environment } from 'src/environments/environment';
     LoginComponent,
   ]
 })
-export class PagesModule { }
+export class LoginModule { }
