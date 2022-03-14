759939b02155e6e8a665f35d687e8b457eea1fa8
jeanellach <jeanellach@gmail.com>
2022-03-14 09:06:31 -0500

    before do the collections for the tables

--- ice-queen/src/app/waiter/tables/tables.component.ts
+++ ice-queen/src/app/waiter/tables/tables.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-tables',
   templateUrl: './tables.component.html',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+
+  ) { }
+
+  addCards() {
+    const buttonAdd = document.querySelector('#btn-add')
+    buttonAdd?.addEventListener('click', async (e) => {
+      e.preventDefault()
+      console.log('funcionaaaaaaaaa')
+      /*let html = "";
+      html +=  ``
+
+    const cardCointainer = document.getElementById('tables-area');
+    if (cardCointainer) cardCointainer.innerHTML = html;*/
+    })
+  }
+
 
   ngOnInit(): void {
   }
